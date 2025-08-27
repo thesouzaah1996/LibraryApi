75a73ee5a334e8499a89eee733c9909c216e435b
Matheus <matheus.de.souza.code@gmail.com>
2025-08-27 12:51:40 -0300

    tabela ator mapeada

---
+++ src/main/java/com/thesouzaah/libraryapi/model/Ator.java
@@ -0,0 +1,27 @@
+package com.thesouzaah.libraryapi.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Data
+@Table(name = "autor", schema = "public")
+public class Ator {
+
+    @Id
+    @Column(name = "codigo_autor")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "nome", length = 100, nullable = false)
+    private String nome;
+
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate dataNascimento;
+
+    @Column(name = "nacionalidade", length = 50, nullable = false)
+    private String nacionalidade;
+}
---
+++ src/main/java/com/thesouzaah/libraryapi/model/Livro.java
@@ -0,0 +1,4 @@
+package com.thesouzaah.libraryapi.model;
+
+public class Livro {
+}
